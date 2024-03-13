618d3ed70e7c1ac38c84366c130a41bd09086aca
GCLEMENT3il <151721756+GCLEMENT3il@users.noreply.github.com>
2024-03-13 13:43:21 +0100

    Création classe LectureFichier
    Fichier qui permet de lire le fichier et de prendre le premier mot est le mettre dans une variable puis prendre le reste de la ligne pour le mettre dans une autre variable

---
+++ LectureFichier.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class LectureFichier {
+
+    public static String[] lireMotEtDefinition(String cheminFichier) throws IOException {
+        ArrayList<String> lignes = lireFichier(cheminFichier);
+        Random rand = new Random();
+        String ligneAleatoire = lignes.get(rand.nextInt(lignes.size()));
+        String[] motsEtDefinition = ligneAleatoire.split("\\s+", 2);
+        return motsEtDefinition;
+    }
+
+    private static ArrayList<String> lireFichier(String cheminFichier) throws IOException {
+        ArrayList<String> lignes = new ArrayList<>();
+        try (BufferedReader lecteur = new BufferedReader(new FileReader(cheminFichier))) {
+            String ligne;
+            while ((ligne = lecteur.readLine()) != null) {
+                lignes.add(ligne);
+            }
+        }
+        return lignes;
+    }
+}
